923543cc1bd447abcab3c4a74558d876a8b79220
mudassir <mudassirsk2006@gmail.com>
2020-04-17 15:44:08 +0530

    test files

---
+++ src/Containers/Dashboard/dashboard.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import Popup from '../../Components/Popup/popup';
+import history from '../Routes/history';
+
+class Dashboard extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			showPopup: false,
+			fields: {},
+			errors: {},
+			productList : [
+				{
+					id: 1,
+					name: 'Television',
+					rate: 100,
+					quality: 10,
+				},
+				{
+					id: 2,
+					name: 'Refrigerator',
+					rate: 200,
+					quality: 30
+				},
+			],
+			popConfig : [ {name:'name', label:'Name'}, {name:'rate', label:'Rate'},{name:'quality', label:'Quality'}]
+		}
+	
+		this.handleChange = this.handleChange.bind(this);
+		this.submitProduct = this.submitProduct.bind(this);
+		this.removeItem = this.removeItem.bind(this);
+	}
+
+	// input change handler
+	handleChange(e) {
+		let fields = this.state.fields;
+		fields[e.target.name] = e.target.value;
+		this.setState({
+			fields
+		});
+	}
+
+	// remove product
+	removeItem(id) {
+		this.setState(prevState => ({
+		productList: prevState.productList.filter(product => product.id !== id)
+		}));
+	}
+
+    // submit / add new product 
+	submitProduct(e) {
+		e.preventDefault();
+		this.displayPopup(false);
+		
+		var newObj = { id : this.state.productList.length + 1, ...this.state.fields}
+		this.setState( prevState => ({
+		...prevState,						
+		productList: [...prevState.productList,  newObj],
+		fields : {}
+		}));
+	}
+
+    // show / hide popup
+	displayPopup(show) {  
+		this.setState({  
+			showPopup: show  
+		})
+	}
+
+
+	render() {
+		const itemStr = localStorage.getItem('greeting');
+		// if user has not come from login redirect him back to login
+		if (!itemStr) {
+			history.push({pathname: '/'});
+		}
+		return (
+			<div id="main-dashboard-container">
+				Hello {localStorage.getItem('greeting')}
+				{this.state.showPopup ?
+					<Popup text='Click "Close Button" to hide popup'  
+						   closePopup={this.displayPopup.bind(this,false)}
+						   changed={this.handleChange}
+						   submitted={this.submitProduct}
+						   fields={this.state.fields}
+						   config={this.state.popConfig}
+					/>
+					: null  
+				}
+				<div>
+					<button onClick={this.displayPopup.bind(this,true)}>Add Product</button>
+				</div><br/>
+			
+				<table border="1">
+				    <thead>
+						<tr>
+						  <th>ID</th>
+						  <th>Name</th>
+						  <th>Rate</th>
+						  <th>Quality</th>
+						  <th></th>
+						</tr>
+					</thead>
+					<tbody>
+					{this.state.productList.map(item => (
+						<tr key={item.id}>
+						<td>{item.id}</td>
+						<td>{item.name}</td>
+						<td>{item.rate}</td>
+						<td>{item.quality}</td>
+						<td><span onClick={() => this.removeItem(item.id)}>Delete</span></td>
+					</tr>
+					))}
+					</tbody>
+				</table>
+			</div>
+		);
+	}
+}
+
+export default Dashboard;
